f6e6a26b067aab9a8e53bbb7631bb02d7f67cc66
up-to-star <2200316527@qq.com>
2025-05-20 03:31:14 +0000

    wmma half naive理解了一点

---
+++ task-1/src/matmul_f16/matmul_f16_v2_wmma_naive.cu
@@ -0,0 +1,56 @@
+#include "playground/system.hpp"
+#include "playground/matmul.hpp"
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <mma.h>
+
+using namespace nvcuda;
+
+#define div_ceil(n,m) (((n) + (m) - 1) / (m))
+
+
+namespace playground
+{
+
+template <typename DType, const int WMMA_M = 16, const int WMMA_N = 16, const int WMMA_K = 16>
+__global__ void hgemm_naive(const DType* __restrict__ A,
+                            const DType* __restrict__ B, DType* __restrict__ C,
+                            const size_t M, const size_t N, const size_t K)
+{
+    const int load_gmem_a_m = blockIdx.y * WMMA_M;
+    const int load_gmem_b_n = blockIdx.x * WMMA_N;
+    const int NUM_K_TILES = div_ceil(K, WMMA_K);
+    if (load_gmem_a_m >= M || load_gmem_b_n >= N)
+        return;
+
+    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, DType, wmma::row_major> a_frag;
+    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, DType, wmma::row_major> b_frag;
+    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, DType> c_frag;
+
+    wmma::fill_fragment(c_frag, 0.0f);
+
+#pragma unroll
+    for (size_t k = 0; k < NUM_K_TILES; k++) {
+        wmma::load_matrix_sync(a_frag, A + load_gmem_a_m * K + k * WMMA_K, K);
+        wmma::load_matrix_sync(b_frag, B + (k * WMMA_K) * N + load_gmem_b_n, N);
+
+        wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
+
+        // __syncthreads();
+    }
+    wmma::store_matrix_sync(C + load_gmem_a_m * N + load_gmem_b_n, c_frag, N, wmma::mem_row_major);
+
+}
+
+PLAYGROUND_MATMUL_SIG(float16_t, 2, M, N, K, A, B, C)
+{
+    const int WMMA_M = 16;
+    const int WMMA_N = 16;
+    const int WMMA_K = 16;
+    dim3 block(32);
+    dim3 grid(div_ceil(N, WMMA_N), div_ceil(M, WMMA_M));
+    hgemm_naive<float16_t, WMMA_M, WMMA_N, WMMA_K><<<grid, block>>>(A, B, C, M, N, K);
+}
+
+}
